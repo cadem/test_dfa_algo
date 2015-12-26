0f93b03549fe7c49f89d769ffa72fe9f361f44a1
Dominic (chendongming) <dongming.chen02@ele.me>
2015-12-27 01:17:32 +0800

    public

--- test_dfa_algo/src/main/java/com/cadem/test/test_dfa_algo/DFAOutCallBack.java
+++ test_dfa_algo/src/main/java/com/cadem/test/test_dfa_algo/DFAOutCallBack.java
@@ -4,7 +4,7 @@ package com.cadem.test.test_dfa_algo;
  * 这是个简单的接口，表示发现匹配关键词后应做的操作
  * keyword:匹配的关键词
  */
-interface DFAOutCallBack {
- public void CallBack(String keyword);
+public interface DFAOutCallBack {
+ void CallBack(String keyword);
 };
 
